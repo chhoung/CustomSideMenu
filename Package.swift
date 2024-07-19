7a110624bfbd9d45cf0d7f2baf7519461317e977
chhorng <phengchhorng@gmail.com>
2024-07-19 14:43:55 +1000

    update package name to avoid conflict

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SideMenu",
+    name: "CustomSideMenu",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SideMenu",
+            name: "CustomSideMenu",
             targets: ["SideMenu"]),
     ],
     dependencies: [
